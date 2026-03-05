455cd2f6a763d619849b41cc3db6824a83d31a05
Katzen48 | Tobi <Katzen48@freenet.de>
2026-03-05 22:12:43 +0100

    fix: use correct base url

--- bundles/schedule/src/extension/index.ts
+++ bundles/schedule/src/extension/index.ts
@@ -6,15 +6,16 @@ export default async (nodecg: NodeCGServerAPI) => {
   set(nodecg); // set nodecg "context" before anything else
 
   setInterval(async () => {
+    const baseUrl = new URL(nodecg.bundleConfig.scheduleUrl ?? 'http://localhost:4001').toString()
     try {
-      const response = await axios.get(nodecg.bundleConfig.scheduleUrl ??  `http://localhost:4001/data/rundowns/current`);
+      const response = await axios.get(new URL('data/rundowns/current', baseUrl).toString());
       nodecg.sendMessage('schedule', response.data);
     } catch (e) {
       console.error(e)
     }
 
     try {
-      const response = await axios.get(nodecg.bundleConfig.scheduleUrl ??  `http://localhost:4001/api/poll`);
+      const response = await axios.get(new URL('api/poll', baseUrl));
       nodecg.sendMessage('event', response.data);
     } catch (e) {
       console.error(e)
