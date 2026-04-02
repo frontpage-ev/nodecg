5250f02adcdfda63d66de25eb9065f548b27e154
Katzen48 | Tobi <Katzen48@freenet.de>
2026-04-03 01:24:20 +0200

    ci: fix typescript bs

--- bundles/schedule/src/graphics/host/Host.vue
+++ bundles/schedule/src/graphics/host/Host.vue
@@ -1,12 +1,13 @@
 <template>
+  <!-- @vue-ignore -->
   <p
     class="font-inter"
     :style="{
       'font-size': fontSize,
       color: fontColor,
       'font-weight': fontWeight,
+      'text-align': align,
       'line-height': '.75em',
-      'text-align': textAlign
     }"
   >
     {{ text }}
@@ -20,7 +21,7 @@ const text = ref<string|null>();
 const fontSize = ref('16px');
 const fontColor = ref('white');
 const fontWeight = ref('400');
-const textAlign = ref('start');
+const align = ref('start');
 
 interface EventData {
   title: string
@@ -48,7 +49,7 @@ const attribute = searchParams.get('attribute') as string
 fontSize.value = searchParams.get('size') as string ?? '16px'
 fontColor.value = searchParams.get('color') as string ?? 'white'
 fontWeight.value = searchParams.get('weight') as string ?? '400'
-textAlign.value = searchParams.get('align') as string ?? 'start'
+align.value = searchParams.get('align') as string ?? 'start'
 
 nodecg.listenFor('event', async ({payload}: {payload: { eventNow: EventData }}) => {
   // @ts-expect-error should be dynamic
