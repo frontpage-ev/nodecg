b941fd7e22eaea692a8e97e880c65f3aa6fb1636
Katzen48 | Tobi <Katzen48@freenet.de>
2026-04-03 00:42:28 +0200

    feat: textAlign in host component

--- bundles/schedule/src/graphics/host/Host.vue
+++ bundles/schedule/src/graphics/host/Host.vue
@@ -6,6 +6,7 @@
       color: fontColor,
       'font-weight': fontWeight,
       'line-height': '.75em',
+      'text-align': textAlign
     }"
   >
     {{ text }}
@@ -19,6 +20,7 @@ const text = ref<string|null>();
 const fontSize = ref('16px');
 const fontColor = ref('white');
 const fontWeight = ref('400');
+const textAlign = ref('start');
 
 interface EventData {
   title: string
@@ -46,6 +48,7 @@ const attribute = searchParams.get('attribute') as string
 fontSize.value = searchParams.get('size') as string ?? '16px'
 fontColor.value = searchParams.get('color') as string ?? 'white'
 fontWeight.value = searchParams.get('weight') as string ?? '400'
+textAlign.value = searchParams.get('align') as string ?? 'start'
 
 nodecg.listenFor('event', async ({payload}: {payload: { eventNow: EventData }}) => {
   // @ts-expect-error should be dynamic
